15cd7ddf1cb9548348c50e0e45f1f41e61d1a630
Cameron Matheson <cameron@instructure.com>
2016-02-20 02:29:32 -0700

    add git config

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ dotfiles[vimrc]=~/.vimrc
 dotfiles[tmux.conf]=~/.tmux.conf
 dotfiles[bashrc]=~/.bashrc
 dotfiles[bash_aliases]=~/.bash_aliases
+dotfiles[gitconfig]=~/.gitconfig
+dotfiles[git_template]=~/.git_template
 
 for f in "$dir"/*; do
   key=$(basename "$f")
