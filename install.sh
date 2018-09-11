d593f086e4cae66ae33131b98f314e6d4be1edc2
Cameron Matheson <cameron.matheson@gmail.com>
2018-09-11 17:05:27 -0600

    add inputrc

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ dotfiles[bashrc]=~/.bashrc
 dotfiles[bash_aliases]=~/.bash_aliases
 dotfiles[gitconfig]=~/.gitconfig
 dotfiles[git_template]=~/.git_template
+dotfiles[inputrc]=~/.inputrc
 
 for f in "$dir"/*; do
   key=$(basename "$f")
